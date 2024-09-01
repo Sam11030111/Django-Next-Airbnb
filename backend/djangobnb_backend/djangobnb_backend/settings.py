29c0f13fc2f2e53828e7cbef5dcf0be7564679db
Sam11030111 <sam86110306@gmail.com>
2024-09-01 10:11:02 -0500

    add host

--- backend/djangobnb_backend/djangobnb_backend/settings.py
+++ backend/djangobnb_backend/djangobnb_backend/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "159.203.41.135"]
 
 AUTH_USER_MODEL = 'useraccount.User'
 
